05cc6081fb9f2f1c5131efd229a739eb43cbf476
Katia <katrusia.tsaruk@gmail.com>
2021-05-19 22:16:05 +0300

    moving paddles

--- AeroHockey/Game.cs
+++ AeroHockey/Game.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using SFML.Graphics;
+using SFML.System;
 using SFML.Window;
 
 namespace AeroHockey
 {
     public class Game
     {
+        private const int windowWidth = 1280;
+        private const int windowHeight = 720;
         private RenderWindow window;
-        Paddle paddle1=new Paddle(true,40,90);
-        Paddle paddle2=new Paddle(false,40,90);
+        Paddle paddle1=new Paddle(true,40,90,windowWidth,windowHeight);
+        Paddle paddle2=new Paddle(false,40,90,windowWidth,windowHeight);
 
         public void Start()
         {
-            RenderWindow window = new RenderWindow(new VideoMode(1280, 720), "Game window");
+            RenderWindow window = new RenderWindow(new VideoMode(windowWidth, windowHeight), "Game window");
             window.Closed += WindowClosed;
 
             while (window.IsOpen)
@@ -21,8 +24,29 @@ namespace AeroHockey
                 window.Clear();
                 window.Draw(paddle1.paddle);
                 window.Draw(paddle2.paddle);
+                MovePaddle();
                 window.DispatchEvents();
                 window.Display();
+            }
+        }
+        public void MovePaddle()
+        {
+            float delta = 0.1f;
+            if (Keyboard.IsKeyPressed(Keyboard.Key.W))
+            {
+                paddle1.ChangePosition(-delta);
+            }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.S))
+            {
+                paddle1.ChangePosition(delta);
+            }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
+            {
+                paddle2.ChangePosition(-delta);
+            }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
+            {
+                paddle2.ChangePosition(delta);
             }
         }
 
@@ -31,7 +55,8 @@ namespace AeroHockey
             RenderWindow w = (RenderWindow)sender;
             w.Close();
         }
-
+        
+        // moglo bi bit krasivo no vidimo net(((
         // private void Background()
         // {
         //     Texture t=new Texture("picture");
--- AeroHockey/Paddle.cs
+++ AeroHockey/Paddle.cs
@@ -1,38 +1,62 @@
-﻿using SFML.Graphics;
+﻿using System;
+using SFML.Graphics;
 using SFML.System;
+using SFML.Window;
 
 namespace AeroHockey
 {
     public class Paddle
     {
         public RectangleShape paddle = new RectangleShape();
-        public bool isPlayer1;
-        public uint sizeX;
-        public uint sizeY;
+        private bool isPlayer1;
+        private int width;
+        private int height;
+        private int windowWidth;
+        private int windowHeight;
 
-        public Paddle(bool isPlayer1, uint sizeX,uint sizeY)
+        public Paddle(bool isPlayer1, int width,int height,int windowWidth,int windowHeight)
         {
             this.isPlayer1 = isPlayer1;
-            this.sizeX = sizeX;
-            this.sizeY = sizeY;
-            SetPaddle();
+            this.width = width;
+            this.height = height;
+            this.windowWidth = windowWidth;
+            this.windowHeight = windowHeight;
+            CreatePaddle();
         }
 
-        public void SetPaddle()
+        public (float,float) GetCurrentPosition()
         {
-            paddle.Size = new Vector2f(sizeX, sizeY);
-            paddle.Origin = new Vector2f(sizeX/2, sizeY/2);
+            return (paddle.Position.X, paddle.Position.Y);
+        }
+
+        private bool TryToChangePosition(float delta)
+        {
+            float newY = paddle.Position.Y + delta;
+            return !(newY <= height / 2 + 10) && !(newY >= windowHeight - height / 2 - 10);
+        }
+        public void ChangePosition(float delta)
+        {
+            if (TryToChangePosition(delta))
+            {
+                paddle.Position = new Vector2f(paddle.Position.X, paddle.Position.Y + delta);
+            }
+        }
+        
+        private void CreatePaddle()
+        {
+            paddle.Size = new Vector2f(width, height);
+            paddle.Origin = new Vector2f(width/2, height/2);
             paddle.OutlineColor=Color.White;
             paddle.OutlineThickness = 3f;
             if (isPlayer1)
             {
                 paddle.FillColor=Color.Blue;
-                paddle.Position=new Vector2f(50, 360);
+                paddle.Position=new Vector2f(50, windowHeight/2);
             }
             else
             {
                 paddle.FillColor=Color.Red;
-                paddle.Position=new Vector2f(1230, 360);
+                paddle.Position=new Vector2f(windowWidth-50, windowHeight/2);
             }
         }
 
